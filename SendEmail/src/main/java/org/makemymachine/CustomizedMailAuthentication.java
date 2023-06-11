63aa91c56c717213873052cdf932921477520c7f
Raushan roy <raushanroyece062@gmail.com>
2023-06-11 14:34:18 +0530

    assignment of send email

---
+++ SendEmail/src/main/java/org/makemymachine/CustomizedMailAuthentication.java
@@ -0,0 +1,13 @@
+package org.makemymachine;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+
+public class CustomizedMailAuthentication extends Authenticator {
+
+    protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(MailMetaData.myUserMail,MailMetaData.password);
+    }
+
+}
---
+++ SendEmail/src/main/java/org/makemymachine/MailMetaData.java
@@ -0,0 +1,17 @@
+package org.makemymachine;
+
+public class MailMetaData {
+
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+    public static final String authPerm = "mail.smtp.auth";
+
+    public static final String myUserMail = "raushanroyece062@gmail.com";
+
+    public static final String password = "arnkyxdhyzpmezay";// app password
+
+    public static final String receiverMail = "raushanroy528@gmail.com";
+
+
+}
